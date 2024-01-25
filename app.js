395f70fb614631f65b1422c4061b6fdd81398b00
fbragee <145098995+fbragee@users.noreply.github.com>
2024-01-25 17:43:17 +0000

    Basic funtionality of the program

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const container = document.querySelector('.container');
+const button = document.querySelector('button');
 
 for (let i = 0; i < 256; i++) {
 	let div = document.createElement('div');
 	div.className = 'box';
 	container.append(div);
+	div.addEventListener('mouseover', () => {
+		div.style.backgroundColor = 'black';
+	});
 }
-
-const box = document.querySelector('.box');
-
-box.addEventListener('hover', () => {});
